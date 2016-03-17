9373059062eb45146f38db23e4515d02ec61776f
brynhildurb12 <brynhildurb12@ru.is>
2016-03-17 13:08:17 -0400

    1c - samvinna BAB ABG
    kóða frá ABG breytt, virkar núna.

--- 1c.rkt
+++ 1c.rkt
@@ -1,15 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1c) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-
-(define (compare n lst)
-  (if(and(not(null? lst)) (equal? n (car lst)))
-     (compare n (cdr lst))
-     lst)
- )
-
 (define (compress lis)
-     (if (null? lis) '()
-      (cons(car lis)(compress(compare (car lis) lis))))
+   (cond
+       ((null? lis) '())
+       ((null? (cdr lis)) lis)
+       ((eq?(car lis)(car (cdr lis))) (compress(cdr lis)))
+       (else (cons(car lis)(compress(cdr lis))))
+   )
 )
+
   
+
+(compress '(a a a b b c c c d d d d e f f))  
