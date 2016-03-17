d3dfe52f32d3e658893396225bf1dba13ab28aa8
brynhildurb12 <brynhildurb12@ru.is>
2016-03-17 12:27:13 -0400

    1c komid
    1c er komið og mooshak samþykkir

--- 1c.rkt
+++ 1c.rkt
@@ -1,14 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1c) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define (compare n lst)
+  (if(and(not(null? lst)) (equal? n (car lst)))
+     (compare n (cdr lst))
+     lst)
+ )
+
 (define (compress lis)
-   (cond
-     ((null? lis) '())
-     ((eq? (car lis)(car (cdr lis))) ((cdr lis)))
-      (else (cons(car lis)(compress(cdr lis))))
-    )
-   )
-
+     (if (null? lis) '()
+      (cons(car lis)(compress(compare (car lis) lis))))
+)
   
-
-(compress '(5 6 7 7 ))
